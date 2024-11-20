4edae118e52660380a67ab2c12e13c3275093302
Asenicon <asa228asa14@gmail.com>
2024-11-20 19:50:45 +0300

    cho-to

--- Assets/Scripts/AnketaScript.cs
+++ Assets/Scripts/AnketaScript.cs
@@ -2,22 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
-public class AnketaScript : MonoBehaviour
-{
+public class AnketaScript : MonoBehaviour {
     public TMP_InputField nameInputField;
     public TMP_Text nameTmpText;
-
-    public void OnButtonUserName()
-    {
+    public TMP_InputField ageInputField;
+    public TMP_Text ageTmpText;
+    private void Start() {
+        nameInputField.onEndEdit.AddListener(NameInputFieldOnEndEdit);
+        ageInputField.onEndEdit.AddListener(AgeInputFieldOnEndEdit);
+    }
+    private void NameInputFieldOnEndEdit(string text) {
+        Debug.Log("AnketaScript::NameInputFieldOnEndEdit(); -- text:" + text);
+        OnButtonUserName();
+    }
+    private void AgeInputFieldOnEndEdit(string text) {
+        Debug.Log("AnketaScript::AgeInputFieldOnEndEdit(); -- text:" + text);
+        OnButtonUserAge();
+    }
+        public void OnButtonUserName() {
+        Debug.Log("AnketaScript::OnButtonUserName(); -- nameInputField.text:" + nameInputField.text);
         nameTmpText.text = nameInputField.text;
     }
-      public TMP_InputField ageInputField;
-      public TMP_Text ageTmpText;
-      
-    // public void OnButtonUserAge()
-    // {
-    //     string ageString = ageInputField.text;
-    //     int ageInt = int.Parse(ageString);
-    // }                                                                                                                                                                               
+    public void OnButtonUserAge() {
+        Debug.Log("AnketaScript::OnButtonUserAge(); -- ageInputField.text:" + ageInputField.text);
+        string ageString = ageInputField.text;
+        if (ageString != null && ageString.Length > 0) {
+            if (int.TryParse(ageString, out int ageInt)) {
+                DateTime dateTime = DateTime.Now;
+                dateTime = dateTime.AddYears(-ageInt);
+                ageTmpText.text = dateTime.ToString("yyyy");
+            } else {
+                ageTmpText.text = "Нужно только число!";
+            }
+        } else {
+            ageTmpText.text = "Не должно быть пустой!";
+        }
+    }
 }
